1875ca0313fcf2a9d85329dd5d82780bdc8e9d1a
Arun Devarajan <arun.d@enfintechnologies.com>
2023-08-09 10:57:02 +0530

    format app.js

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 //db connection
- async function dbconnect(){
+async function dbconnect() {
   await mongoose.connect(process.env.MongoDBurl)
-
- }
- dbconnect()
-.then(() => console.log('DB connected.'))
-.catch(err => console.log(err));
- const {tokenVerification}  = require('./middlewares/tokenVerification')
+}
+dbconnect()
+  .then(() => console.log('DB connected.'))
+  .catch(err => console.log(err));
+const { tokenVerification } = require('./middlewares/tokenVerification')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const booksRouter=require("./routes/books");
+const booksRouter = require("./routes/books");
 // var crudRouter= require('./routes/crud');
 var app = express();
 
@@ -32,17 +31,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/books',tokenVerification,booksRouter)
+app.use('/books', tokenVerification, booksRouter)
 // app.use('createRecord',crudRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   // res.locals.message = err.message;
   // res.locals.error = req.app.get('env') === 'development' ? err : {};
